b7f1e25454a0cadd1367b4575dcbd4b3b63a3c1a
bob <bob@APCSA>
2018-09-06 15:35:54 -0700

    Started working on the TwoIntegers project.

---
+++ src/TwoIntegers.java
@@ -0,0 +1,25 @@
+
+public class TwoIntegers 
+{
+	public int num1;
+	public int num2;
+	
+	public TwoIntegers()
+	{
+		num1 = 0;
+		num2 = 0;
+	}
+	public TwoIntegers(int num1, int num2)
+	{
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	public int getNum1()
+	{
+		return num1;
+	}
+	public void setNum1(int num1)
+	{
+		this.num1 = num1;
+	}	
+}
