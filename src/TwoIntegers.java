aded299ba0d4c02279b61adf02a690090c9907b3
bob <bob@APCSA>
2018-09-14 15:37:57 -0700

    Finished 2.16 and 2.25 in two integers.

--- src/TwoIntegers.java
+++ src/TwoIntegers.java
@@ -44,5 +44,26 @@ public class TwoIntegers
 				num1 + " / " + num2 + " = " + quot + ", " +
 				num1 + " % " + num2 + " = " + mod);
 	}
-	
+	public int larger()
+	{
+		if (num1 >= num2)
+		{
+			return num1;
+		}
+		else
+		{
+			return num2;
+		}
+	}
+	public boolean isEven()
+	{
+		if((num1 + num2) % 2 == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}	
+	}
 }
