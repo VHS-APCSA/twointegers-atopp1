912a0b567f2364534c993d7464bbc0c80591ba7e
bob <bob@APCSA>
2018-09-19 14:54:28 -0700

    Finishing two integers runner.

--- src/TIRunner.java
+++ src/TIRunner.java
@@ -1,9 +1,16 @@
-
+import java.util.scanner
 public class TIRunner 
 {
 	public static void main (String []args)
 	{
-TwoIntegers twoIntegers = new TwoIntegers(7, 8);
+		Scanner input = new Scanner (System.in);
+		System.out.print("Enter first integer");
+		int input1 = input.nextInt();
+		System.out.print("Enter second integer");
+		int input2 = input.nextInt();
+		System.out.println("The integers are");
+		
+		TwoIntegers twoIntegers = new TwoIntegers(7, 8);
 String arithmetic = twoIntegers.arithmetic();
 System.out.println(arithmetic);
 }
